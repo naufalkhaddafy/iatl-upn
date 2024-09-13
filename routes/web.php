a1f0e7b043623cc0167aa1c1396320282127fc62
naufalkhaddafy <naufalkhaddafy@webmail.umm.ac.id>
2024-09-14 06:14:50 +0800

    fix: setup middleware for role admin

--- routes/web.php
+++ routes/web.php
@@ -42,19 +42,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])
         ->name('dashboard')
         ->middleware('auth');
-    //News
-    Route::resource('/news', NewsController::class);
-
-    //Users
-    Route::resource('/user', UserController::class);
-    Route::get('/list-alumni', [UserController::class, 'indexAlumni'])->name('admin.index.alumni');
-    Route::get('/verifikasi/alumni', [UserController::class, 'verifikasiAlumni'])->name('admin.verifikasi.alumni');
-    Route::put('/verifikasi/alumni/{user}', [UserController::class, 'verifikasiAlumniUpdate'])->name('admin.verifikasi.alumni.update');
-    Route::get('/list-admin', [UserController::class, 'indexAdmin'])->name('admin.index.admin');
-
-    Route::get('/admin/export/alumni/{type}', [UserController::class, 'export'])->name('admin.export.alumni');
-    //Web Settings
-    Route::get('/web-settings', [ContentController::class, 'index'])->name('web.settings');
+    Route::group(['middleware' => ['role:admin']], function () {
+        //News
+        Route::resource('/news', NewsController::class);
+        //Users
+        Route::resource('/user', UserController::class);
+        Route::get('/list-alumni', [UserController::class, 'indexAlumni'])->name('admin.index.alumni');
+        Route::get('/verifikasi/alumni', [UserController::class, 'verifikasiAlumni'])->name('admin.verifikasi.alumni');
+        Route::put('/verifikasi/alumni/{user}', [UserController::class, 'verifikasiAlumniUpdate'])->name('admin.verifikasi.alumni.update');
+        Route::get('/list-admin', [UserController::class, 'indexAdmin'])->name('admin.index.admin');
+        Route::get('/admin/export/alumni/{type}', [UserController::class, 'export'])->name('admin.export.alumni');
+        //Web Settings
+        Route::get('/web-settings', [ContentController::class, 'index'])->name('web.settings');
+    });
 
     // Sebaran
     Route::get('/sebaran/alumni', [SebaranController::class, 'index'])->name('sebaran.alumni');
