0aa565581a719420459ecf2ac5346fd5f7a0c278
naufalkhaddafy <naufalkhaddafy@webmail.umm.ac.id>
2024-09-23 21:25:12 +0800

    fix: commit late fix update user

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -22,24 +22,23 @@ class UserRequest extends FormRequest
      */
     public function rules(): array
     {
-         return [
-            'name'=> 'required|min:2|max:255',
-            'email'=> ['required','email', Rule::unique('users','email')->ignore($this->user?->id)],
-            'nim'=> ['numeric', $this->user?->getRoleNames()[0] == 'admin'  ? 'nullable' : 'required', Rule::unique('users','nim')->ignore($this->user?->id)],
-            'generation'=> ['numeric', $this->method() !== 'POST'  && $this->user?->getRoleNames()[0] !== 'admin' ? 'required' : 'nullable'],
-            'phone_number'=> [$this->method() !== 'POST'  && $this->user?->getRoleNames()[0] !== 'admin' ? 'required' : 'nullable','digits_between:7,15','numeric' ],
-            'domicile' => [ $this->method() !== 'POST'  && $this->user?->getRoleNames()[0] !== 'admin' ? 'required' : 'nullable','max:255'],
-            'address_now' => [$this->method() !== 'POST' && $this->user?->getRoleNames()[0] !== 'admin' ? 'required' : 'nullable','max:255'],
-            'domicile_id'=>[ $this->method() !== 'POST'  && $this->user?->getRoleNames()[0] !== 'admin' ? 'required' : 'nullable'],
-            'address_now_id'=>[$this->method() !== 'POST'  && $this->user?->getRoleNames()[0] !== 'admin' ? 'required' : 'nullable'],
-            'image'=>['nullable', 'max:2000','mimes:png,jpg,jpeg'],
-            'motto'=>'nullable',
-            'goal'=>'nullable',
-            'company_name'=>'nullable',
-            'company_address'=>'nullable',
-            'position'=>'nullable',
-            'password' => ['sometimes','required','min:5', 'confirmed'],
+        return [
+            'name' => 'required|min:2|max:255',
+            'email' => ['required', 'email', Rule::unique('users', 'email')->ignore($this->user?->id)],
+            'nim' => ['numeric', $this->user?->getRoleNames()[0] == 'admin' ? 'nullable' : 'required', Rule::unique('users', 'nim')->ignore($this->user?->id)],
+            'generation' => [$this->method() !== 'POST' && $this->user?->getRoleNames()[0] !== 'admin' ? 'required' : 'nullable','digits:4','numeric'],
+            'phone_number' => [$this->method() !== 'POST' && $this->user?->getRoleNames()[0] !== 'admin' ? 'required' : 'nullable', 'digits_between:7,15', 'numeric'],
+            'domicile' => [$this->method() !== 'POST' && $this->user?->getRoleNames()[0] !== 'admin' ? 'required' : 'nullable', 'max:255'],
+            'address_now' => [$this->method() !== 'POST' && $this->user?->getRoleNames()[0] !== 'admin' ? 'required' : 'nullable', 'max:255'],
+            'domicile_id' => [$this->method() !== 'POST' && $this->user?->getRoleNames()[0] !== 'admin' ? 'required' : 'nullable'],
+            'address_now_id' => [$this->method() !== 'POST' && $this->user?->getRoleNames()[0] !== 'admin' ? 'required' : 'nullable'],
+            'image' => ['nullable', 'max:2000', 'mimes:png,jpg,jpeg'],
+            'motto' => 'nullable',
+            'goal' => 'nullable',
+            'company_name' => 'nullable',
+            'company_address' => 'nullable',
+            'position' => 'nullable',
+            'password' => [$this->method() !== 'POST' ? 'nullable' : 'required', 'min:5', 'confirmed'],
         ];
-
     }
 }
